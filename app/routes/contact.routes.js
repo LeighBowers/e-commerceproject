3017f77d9ba59c58a248ed0fb8a63d2a5b62b01c
Leigh-Anne <leighsylvester9@gmail.com>
2022-03-18 15:20:05 +0200

    nodemailer

--- app/routes/contact.routes.js
+++ app/routes/contact.routes.js
@@ -1,10 +1,52 @@
-const express = require('express');
-const app = express.Router();
-const nodemailer = require('nodemailer');
-require("dotenv").config();
-const User = require("../models/user");
-const user =require("./user.Routes")
+// const express = require('express');
+// const app = express.Router();
+// const nodemailer = require('nodemailer');
+// require("dotenv").config();
+// const User = require("../models/user");
+// const user =require("./user.Routes")
+const express = require("express")
 
+const nodemailer = require('nodemailer');
+require("dotenv").config()
+const app = express.Router()
+
+app.post('/', (req, res) => {
+    let { name, email, phone, subject } = req.body
+
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS
+        }
+    });
+
+
+    const mailOptions = {
+        from: email,
+        to: 'leighsylvester9@gmail.com',
+        subject: 'New contact from your portfolio',
+        text: `${name} has contact you please contact them back on ${phone}
+         ${subject}`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+
+        if (error) {
+            console.log(error);
+            res.status(400).send({ msg: "Email not send" });
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send({ msg: "Email has been sent successfully" });
+        }
+    });
+
+})
+module.exports = app
 
 app.get('/', (req, res) => {
     res.send({ message: "For Contact only POST method accepted" })
