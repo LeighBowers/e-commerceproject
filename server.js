7c65b729beb89ca99ea36fcb7741699b156bd03e
Leigh-Anne <leighsylvester9@gmail.com>
2022-03-22 12:46:44 +0200

    mailer

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const productsRouter = require("./app/routes/product.routes");
 const usersRouter = require("./app/routes/user.routes");
 const cartRouter = require("./app/routes/cart.routes");
+const contactRouter = require("./app/routes/contact.routes");
 // ("./routes/products.routes");
 // ("./routes/users.routes");
 const app = express();
@@ -26,6 +27,6 @@ app.get('/', (req, res) => {
 app.use("/products", productsRouter);
 app.use("/users", usersRouter);
 app.use("/cart", cartRouter);
-app.use("/contact", cartRouter);
+app.use("/contact", contactRouter);
 
 app.listen(app.get("port"), () => console.log(" Server started "));
